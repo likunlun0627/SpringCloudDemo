76719279b215c3f4c188874aff6fe1207320e949
likunlun0627 <1540799439@qq.com>
2021-12-14 14:25:36 +0800

    Config-20211214 14:24

--- ConfigClient/src/main/java/com/cloud/config/client/ConfigClientApplication.java
+++ ConfigClient/src/main/java/com/cloud/config/client/ConfigClientApplication.java
@@ -3,17 +3,20 @@ package com.cloud.config.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.bootstrap.BootstrapConfiguration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 @EnableEurekaClient
-@BootstrapConfiguration
+@RefreshScope
 @RestController
 public class ConfigClientApplication {
 
+    Logger log = Logger.getLogger(ConfigClientApplication.class.getName());
     @Value("${info.profile}")
     private String profile;
     @Value("${name}")
@@ -27,6 +30,9 @@ public class ConfigClientApplication {
 
     @RequestMapping("/info")
     public String info() {
+        log.info("\tprofile：" + profile
+                + "\n\tname：" + name
+                + "\n\tdesc：" + desc);
         return "profile：" + profile + "<br>"
                 + "name：" + name + "<br>"
                 + "desc：" + desc;
