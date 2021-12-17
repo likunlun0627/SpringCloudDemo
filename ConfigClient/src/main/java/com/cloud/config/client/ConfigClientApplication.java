9c51c034b80be07013867f10c163df23e1a297b7
likunlun0627 <1540799439@qq.com>
2021-12-17 18:03:04 +0800

    Spring Boot Admin-20211217 18:00

--- ConfigClient/src/main/java/com/cloud/config/client/ConfigClientApplication.java
+++ ConfigClient/src/main/java/com/cloud/config/client/ConfigClientApplication.java
@@ -3,15 +3,15 @@ package com.cloud.config.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.logging.Logger;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @RefreshScope
 @RestController
 public class ConfigClientApplication {
@@ -30,7 +30,7 @@ public class ConfigClientApplication {
 
     @RequestMapping("/info")
     public String info() {
-        log.info("\tprofile：" + profile
+        log.info("\n\tprofile：" + profile
                 + "\n\tname：" + name
                 + "\n\tdesc：" + desc);
         return "profile：" + profile + "<br>"
